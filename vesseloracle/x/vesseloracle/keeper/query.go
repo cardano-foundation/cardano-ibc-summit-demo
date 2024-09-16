99c94db501763d7236bccecde8bd12b3e8ce1d41
Sebastian Bode <sebastian.bode@cardanofoundation.org>
2024-09-16 10:53:53 +0200

    chore: Renamed Cosmos chain

---
+++ vesseloracle/x/vesseloracle/keeper/query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"vesseloracle/x/vesseloracle/types"
+)
+
+var _ types.QueryServer = Keeper{}
---
+++ vesseloracle/x/vesseloracle/module/genesis_test.go
@@ -0,0 +1,30 @@
+package vesseloracle_test
+
+import (
+	"testing"
+
+	keepertest "vesseloracle/testutil/keeper"
+	"vesseloracle/testutil/nullify"
+	vesseloracle "vesseloracle/x/vesseloracle/module"
+	"vesseloracle/x/vesseloracle/types"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.VesseloracleKeeper(t)
+	vesseloracle.InitGenesis(ctx, k, genesisState)
+	got := vesseloracle.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
